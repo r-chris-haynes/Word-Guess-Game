f027b795c3572a5e5e2e1cb05c9d4164e905ec68
chris haynes <r.chris.haynes@gmail.com>
2019-09-12 19:20:37 -0400

    displaying correct choice and wrong in HTML

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,26 @@
-var nameOptions = ["homer", "marge", "lisa", "bart", "maggie", "grampa",
-                   "ned", "monty", "nelson", "mihouse", "barney", "smithers",
-                    "edna", "lenny", "moe", "apu", "krusty", "skinner", "willie",
-                    "pattie", "wiggum"];
+var nameOptions = [
+  "homer",
+  "marge",
+  "lisa",
+  "bart",
+  "maggie",
+  "grampa",
+  "ned",
+  "monty",
+  "nelson",
+  "mihouse",
+  "barney",
+  "smithers",
+  "edna",
+  "lenny",
+  "moe",
+  "apu",
+  "krusty",
+  "skinner",
+  "willie",
+  "pattie",
+  "wiggum"
+];
 
 var selectedName = "";
 var lettersInName = [];
@@ -13,55 +32,66 @@ var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
 
-function startGame () {
-    selectedName = nameOptions[Math.floor(Math.random() * nameOptions.length)];
-    lettersInName = selectedName.split("");
-    numBlanks = lettersInName.length;
+function startGame() {
+  selectedName = nameOptions[Math.floor(Math.random() * nameOptions.length)];
+  lettersInName = selectedName.split("");
+  numBlanks = lettersInName.length;
 
-    blanksAndSuccesses = [];
-    wrongLetters = [];
-    guessesLeft = 9;
+  blanksAndSuccesses = [];
+  wrongLetters = [];
+  guessesLeft = 9;
 
-    for (var i = 0; i < numBlanks; i++) {
-        blanksAndSuccesses.push("_");
-    }
+  for (var i = 0; i < numBlanks; i++) {
+    blanksAndSuccesses.push("_");
+  }
 
-    document.getElementById("nameToGuess").innerHTML = blanksAndSuccesses.join(" ");
-    document.getElementById("numGuesses").innerHTML = guessesLeft;
-    document.getElementById("winCounter").innerHTML = wins;
-    document.getElementById("lossCounter").innerHTML = losses;
+  document.getElementById("nameToGuess").innerHTML = blanksAndSuccesses.join(
+    " "
+  );
+  document.getElementById("numGuesses").innerHTML = guessesLeft;
+  document.getElementById("winCounter").innerHTML = wins;
+  document.getElementById("lossCounter").innerHTML = losses;
 
-    
-    console.log(selectedName);
-    console.log(lettersInName);
-    console.log(numBlanks);
-    console.log(blanksAndSuccesses);
+  console.log(selectedName);
+  console.log(lettersInName);
+  console.log(numBlanks);
+  console.log(blanksAndSuccesses);
 }
 
 function checkLetters(letter) {
-   
-    var isLetterInName = false;
-    
-    for (var i = 0; i < numBlanks; i++) {
-        if (selectedName[i] == letter) {
-            isLetterInName = true;
-            alert("letter found")
-        }
+  var isLetterInName = false;
+
+  for (var i = 0; i < numBlanks; i++) {
+    if (selectedName[i] == letter) {
+      isLetterInName = true;
+      console.log(isLetterInName);
+      console.log("letter found");
     }
-    
-    
+  }
+
+  if (isLetterInName) {
+    for (var i = 0; i < numBlanks; i++) {
+      if (selectedName[i] == letter) {
+        blanksAndSuccesses[i] = letter;
+        console.log("test")
+      }
+    }
+  }
+  else {
+      wrongLetters.push(letter);
+      guessesLeft --;
+      console.log(guessesLeft);
+      document.getElementById("numGuesses").innerHTML = guessesLeft;   
+  }
+ document.getElementById("nameToGuess").innerHTML = blanksAndSuccesses.join(
+    " ");
 }
 
 startGame();
 
 document.onkeyup = function(event) {
-    var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
-    checkLetters(letterGuessed);
-    
-    console.log(letterGuessed);
-}
+  var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
+  checkLetters(letterGuessed);
 
-
-
-
-
+  console.log(letterGuessed);
+};
